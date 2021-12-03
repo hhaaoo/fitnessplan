527fd03d002b96f5f2f304415703baa2e494659f
hhaaoo <hhaaoowwuu@gmail.com>
2021-12-02 20:11:42 -0500

    initial setup

---
+++ fitnessplan/fitnessplan/fitnessplanApp.swift
@@ -0,0 +1,17 @@
+//
+//  fitnessplanApp.swift
+//  fitnessplan
+//
+//  Created by Hao Wu on 2021-12-02.
+//
+
+import SwiftUI
+
+@main
+struct fitnessplanApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
